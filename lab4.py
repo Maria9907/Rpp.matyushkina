431edfd7e109ed5b07d13f10121653de698aecc5
Матюшкина Мария <maria18112005@yandex.ru>
2026-04-04 11:34:30 +0700

    html-4 работа

---
+++ lab4.py
@@ -0,0 +1,104 @@
+
+from flask_login import LoginManager
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'lab4-secret-key-2026'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+#Главная страница для авторизованных пользователей
+@app.route('/')
+@login_required
+def index():
+    return render_template('index.html', user=current_user)
+
+#Страница входа
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        if not email or not password:
+            flash('Пожалуйста, заполните все поля', 'error')
+            return redirect(url_for('login'))
+        
+        user = User.query.filter_by(email=email).first()
+        
+        if not user:
+            flash('Пользователь с таким email не найден', 'error')
+            return redirect(url_for('login'))
+        
+        if not check_password_hash(user.password, password):
+            flash('Неверный пароль', 'error')
+            return redirect(url_for('login'))
+        
+        login_user(user)
+        flash(f'Добро пожаловать, {user.name}!', 'success')
+        return redirect(url_for('index'))
+    
+    return render_template('login.html')
+
+#Страница регистрации
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        if not name or not email or not password:
+            flash('Пожалуйста, заполните все поля', 'error')
+            return redirect(url_for('signup'))
+        
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            flash('Пользователь с таким email уже существует', 'error')
+            return redirect(url_for('signup'))
+        
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+        
+        new_user = User(
+            name=name,
+            email=email,
+            password=hashed_password
+        )
+        
+        db.session.add(new_user)
+        db.session.commit()
+        
+        flash('Регистрация прошла успешно! Теперь вы можете войти в систему.', 'success')
+        return redirect(url_for('login'))
+    
+    return render_template('signup.html')
+
+#Выход
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Вы успешно вышли из системы', 'info')
+    return redirect(url_for('login'))
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
